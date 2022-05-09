55880711bb9d25eaea2103af8406240623b1d048
Zhilin21-IST-4 <94217280+Zhilin21-IST-4@users.noreply.github.com>
2022-05-09 15:57:23 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,6 +4,28 @@
 
 template<typename T, int size>
 class TStack {
+ private:
+        T arr[100];
+        int top1;
 
+ public:
+    TStack(): top1(-1) { }
+    T get() const {
+      return arr[top1];
+    }
+    bool isEmpty() const {
+      return top1 == -1;
+    }
+    bool isFull() const {
+      return top1 == size - 1;
+    }
+    void pop() {
+      if (top1 >= 0)
+        top1--;
+      }
+    void push(T item) {
+       if (top1 < size - 1)
+         arr[++top1] = item;
+       }
 };
 #endif  // INCLUDE_TSTACK_H_
